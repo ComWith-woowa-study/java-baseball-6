0d48d56c788bbc5d4c4133cdd3f3f30e8f49538a
smini <sm020320@naver.com>
2025-09-16 18:58:37 +0900

    feat: Ball 객체 생성

---
+++ src/main/java/baseball/model/Ball.java
@@ -0,0 +1,23 @@
+package baseball.model;
+
+import java.util.List;
+
+public class Ball {
+
+    private static final int MIN = 1, MAX = 9;
+    private final List<Integer> ball;
+
+    public Ball(List<Integer> numbers) {
+        if (!numbers.stream().allMatch(n -> n >= MIN && n <= MAX)){
+            throw new IllegalArgumentException("모든 숫자는 " + MIN + "~" + MAX + " 사이여야 합니다.");
+        }
+        if (numbers.size() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException("숫자는 중복될 수 없습니다.");
+        }
+        this.ball = List.copyOf(numbers);
+    }
+
+    public List<Integer> getBall() {
+        return ball;
+    }
+}
