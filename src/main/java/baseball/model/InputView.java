d949927f3ca66bcc74e8d615752694a23d8b57cd
smini <sm020320@naver.com>
2025-09-16 22:54:01 +0900

    refactor: InputView - Scanner 대신 Console 사용

--- src/main/java/baseball/model/InputView.java
+++ src/main/java/baseball/model/InputView.java
@@ -1,16 +1,16 @@
 package baseball.model;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
 
-    private final Scanner scanner = new Scanner(System.in);
-
     public List<Integer> inputNumbers() {
         System.out.println("숫자를 입해주세요 :");
-        String userBall = scanner.nextLine();
+        String userBall = Console.readLine();
         if (userBall == null || userBall.isEmpty()) {
             throw new IllegalArgumentException("공이 없습니다.");
         }
