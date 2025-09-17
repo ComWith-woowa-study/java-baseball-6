47176256ee15a4622722f102e91e3573501683b2
smini <sm020320@naver.com>
2025-09-17 19:33:09 +0900

    feat: Result 객체 출력 메서드 추가

--- src/main/java/baseball/model/Result.java
+++ src/main/java/baseball/model/Result.java
@@ -20,4 +20,15 @@ public class Result {
     public int getNothing() {
         return nothing;
     }
+
+    @Override
+    public String toString() {
+        if (strike == 0 && ball == 0) {
+            return "낫싱";
+        }
+        if (strike == 3 && ball == 0) {
+            return "3스트라이크";
+        }
+        return ball + "볼 " + strike + "스트라이크";
+    }
 }
