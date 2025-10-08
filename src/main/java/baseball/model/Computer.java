440c456d0b3b5be7b0835dc8bbabd91b1b6018d6
smini <sm020320@naver.com>
2025-10-08 19:32:15 +0900

    refactor(model): 상태 제거 및 Ball 생성 책임 명확화

--- src/main/java/baseball/model/Computer.java
+++ src/main/java/baseball/model/Computer.java
@@ -7,16 +7,12 @@ import java.util.List;
 
 public class Computer {
 
-    private final List<Integer> computer = new ArrayList<>();
-
-    public void setComputer() {
-        computer.clear();
-        while (computer.size() < Ball.BALL_SIZE) {
+    public Ball generateNumbers() {
+        List<Integer> numbers = new ArrayList<>();
+        while (numbers.size() < Ball.BALL_SIZE) {
             int randomNumber = Randoms.pickNumberInRange(Ball.MIN, Ball.MAX);
-            if (!computer.contains(randomNumber)) computer.add(randomNumber);
+            if (!numbers.contains(randomNumber)) { numbers.add(randomNumber); }
         }
-    }
-    public List<Integer> getComputer() {
-        return List.copyOf(computer);
+        return new Ball(numbers);
     }
 }
