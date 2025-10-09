f49c612ee8c9b3db4321cedfcef938ab9bf8321c
smini <sm020320@naver.com>
2025-10-09 13:51:38 +0900

    refactor(model): 책임 분리 및 랜덤 변수 인터페이스 주입

--- src/main/java/baseball/model/Computer.java
+++ src/main/java/baseball/model/Computer.java
@@ -1,18 +1,19 @@
 package baseball.model;
 
-import camp.nextstep.edu.missionutils.Randoms;
-
-import java.util.ArrayList;
+import baseball.generate.NumberGenerator;
+import baseball.generate.RandomNumberGenerator;
 import java.util.List;
 
 public class Computer {
 
+    private NumberGenerator numberGenerator;
+
+    public Computer(NumberGenerator numberGenerator) {
+        this.numberGenerator = numberGenerator;
+    }
+
     public Ball generateNumbers() {
-        List<Integer> numbers = new ArrayList<>();
-        while (numbers.size() < Ball.BALL_SIZE) {
-            int randomNumber = Randoms.pickNumberInRange(Ball.MIN, Ball.MAX);
-            if (!numbers.contains(randomNumber)) { numbers.add(randomNumber); }
-        }
-        return new Ball(numbers);
+        List<Integer> randomNumber = numberGenerator.generate();
+        return new Ball(randomNumber);
     }
 }
