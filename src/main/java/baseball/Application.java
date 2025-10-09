b21bc30312bdf3dcfe70db00cc3928f174fc5b39
smini <sm020320@naver.com>
2025-10-09 15:37:29 +0900

    refactor(application): 게임 실행 로직을 MVC 구조로 분리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,36 +1,19 @@
 package baseball;
 
+import baseball.controller.BaseballGameController;
+import baseball.generate.RandomNumberGenerator;
 import baseball.model.*;
-import camp.nextstep.edu.missionutils.Console;
-import java.util.List;
+import baseball.view.InputView;
+import baseball.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-
-        Computer computer = new Computer();
-        Player player = new Player();
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
         Umpire umpire = new Umpire();
+        Computer computer = new Computer(new RandomNumberGenerator(Ball.BALL_SIZE, Ball.MIN, Ball.MAX));
 
-        System.out.println("숫자 야구 게임을 시작합니다.");
-        while (true) {
-            computer.setComputer();
-            Ball computerBall = new Ball(computer.getComputer());
-            while (true) {
-                List<Integer> user = player.inputNumbers();
-                Ball playerBall = new Ball(user);
-
-                Result gameResult = umpire.gameStart(playerBall, computerBall);
-
-                if (gameResult.getStrike() == Ball.BALL_SIZE) {
-                    System.out.println(gameResult);
-                    System.out.println(Ball.BALL_SIZE + "개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                    System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                    String userResponse = Console.readLine();
-                    if (userResponse.equals("1")) break;
-                    if (userResponse.equals("2")) return;
-                    throw new IllegalArgumentException("1 또는 2를 입력해야 합니다.");
-                } else System.out.println(gameResult);
-            }
-        }
+        BaseballGameController gameController = new BaseballGameController(inputView, outputView, computer, umpire);
+        gameController.gameStart();
     }
 }
