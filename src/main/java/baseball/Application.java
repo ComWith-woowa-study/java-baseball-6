1ce406bce86b5bd2c85cd49fa5d2a19e0ddb7295
박병욱 <114344042+Rockernun@users.noreply.github.com>
2025-10-10 16:20:43 +0900

    refactor(app): Application 메인에서 DefaultGameMediator 기반으로 실행 플로우 단순화
    - Player, Computer 인스턴스 직접 생성
    - DefaultGameMediator에 주입 후 mediator.start() 호출

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,20 +1,16 @@
 package baseball;
 
 import baseball.computer.Computer;
-import baseball.computer.Machine;
-import baseball.exception.GameRules;
 import baseball.mediator.DefaultGameMediator;
-import baseball.mediator.GameMediator;
 import baseball.player.Player;
 
 public class Application {
 
     public static void main(String[] args) {
 
-        GameRules rules = new GameRules(3, 1, 9);
-        Player player = new Player(rules);
-        Machine computer = new Computer(rules);
-        GameMediator mediator = new DefaultGameMediator(rules, computer, player);
+        Player player = new Player();
+        Computer computer = new Computer();
+        DefaultGameMediator mediator = new DefaultGameMediator(computer, player);
 
         mediator.start();
     }
