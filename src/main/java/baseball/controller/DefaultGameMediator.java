f7b285108bc00dfcbab6cbdd11324e16823c5478
박병욱 <114344042+Rockernun@users.noreply.github.com>
2025-10-11 09:32:45 +0900

    refactor(controller): 입력 예외 처리 수정
    - 도메인 로직과 관계없는 예외 처리만 InputView로 분리하는 것으로 처리

--- src/main/java/baseball/controller/DefaultGameMediator.java
+++ src/main/java/baseball/controller/DefaultGameMediator.java
@@ -5,7 +5,10 @@ import baseball.model.Score;
 import baseball.view.InputView;
 import baseball.view.OutputView;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static baseball.model.GameConfig.*;
 
@@ -26,7 +29,7 @@ public class DefaultGameMediator {
         output.printStart();
         while (true) {
             output.printGuess();
-            List<Integer> guess = input.getInput();
+            List<Integer> guess = getInput();
             Score score = computer.judge(guess);
 
             if (score.isNothing()) {
@@ -37,12 +40,57 @@ public class DefaultGameMediator {
 
             if (score.isWin()) {
                 output.printResult(SIZE, GO, STOP);
-                if (input.getRestartOrStop()) {
+                if (getRestartOrStop()) {
                     computer.generate();
                 } else {
                     break;
                 }
             }
         }
+    }
+
+    public List<Integer> getInput() {
+        String line = input.readLine();
+
+        if (line.length() != SIZE) {
+            throw new IllegalArgumentException(String.format("정확히 %d자리를 입력해야 합니다!", SIZE));
+        }
+
+        List<Integer> list = new ArrayList<>();
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < SIZE; i++) {
+            char c = line.charAt(i);
+            if (!Character.isDigit(c)) {
+                throw new IllegalArgumentException( String.format("공백 없이 %d ~ %d 사이에 존재하는 숫자 중 %d자리를 입력해야 합니다!" , MIN, MAX, SIZE) );
+            }
+
+            int number = c - '0';
+            if (number < MIN || number > MAX) {
+                throw new IllegalArgumentException(String.format("%d ~ %d 범위의 숫자만 입력할 수 있습니다!", MIN, MAX));
+            }
+
+            if (!set.add(number)) {
+                throw new IllegalArgumentException("숫자가 중복되었습니다!");
+            }
+
+            list.add(number);
+        }
+        return list;
+    }
+
+    public boolean getRestartOrStop() {
+        String restartOrStop = input.readLine();
+        int number = Integer.parseInt(restartOrStop);
+
+        if (restartOrStop.isEmpty()) {
+            throw new IllegalArgumentException("게임 재개 여부를 입력해주세요!");
+        }
+
+        return switch(number) {
+            case 1 -> true;
+            case 2 -> false;
+            default -> throw new IllegalArgumentException(String.format("%d 또는 %d만 입력할 수 있습니다!", GO, STOP));
+        };
     }
 }
